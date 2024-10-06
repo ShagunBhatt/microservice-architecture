5316e2790e7ca3de16edabd59da47da5242fafdc
Shagun <shagunbhatt94@gmail.com>
2024-10-06 22:10:34 +0530

    Added gitignore

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DemoApplication {
+	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
